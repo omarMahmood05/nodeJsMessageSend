9613529b3498e1235dc2ed6455295688d5c91052
Omar Mahmood <omarmahmood005@gmail.com>
2023-04-02 20:44:36 +0530

    added site

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-const http = require('http');
+const dotenv = require("dotenv-vault-core").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!");
+});
+
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: process.env.MAILID,
+    pass: process.env.PASS,
+  },
+});
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.post("/", (req, res) => {
+  const userName = req.body.userName;
+  const message = req.body.userMessage;
+
+  console.log(process.env.MAILID);
+  console.log(process.env.PASS);
+
+  const options = {
+    from: "omarnodejs@outlook.com",
+    to: "omarmahmood005@gmail.com",
+    subject: `Message from: ${userName}`,
+    text: message,
+  };
+
+  transporter.sendMail(options, (err, info) => {
+    if (!err) {
+      res.redirect("/sent");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+server.get("/sent", (req, res) => {
+  res.sendFile(__dirname + "/sent.html");
+});
+
+server.post("/home", (req, res) => {
+  res.redirect("/");
 });
 
 server.listen(PORT, () => {
